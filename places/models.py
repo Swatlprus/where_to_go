54859833a889f3456362e2cf5b1d63830484e516
Rinat <swatlprus@gmail.com>
2023-04-28 17:28:36 +0500

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -5,8 +5,8 @@ class Places(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     description_short = models.CharField(max_length=250, verbose_name='Краткое описание')
     description_long = models.TextField(verbose_name='Полное описание')
+    lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
-    lon = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
