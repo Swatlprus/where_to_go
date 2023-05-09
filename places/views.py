da09dbe1daaa48e223d37ce22ba3f35eb78cf780
Rinat <swatlprus@gmail.com>
2023-05-09 14:45:15 +0500

    Fixed quotes

--- places/views.py
+++ places/views.py
@@ -8,13 +8,13 @@ def get_desciptions_place(requests, place_id):
     place = get_object_or_404(Places, pk=place_id)
 
     place_point = {
-        "title": place.title,
-        "imgs": [image.img.url for image in place.img.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-                    "lat": place.lat,
-                    "lng": place.lng,
+        'title': place.title,
+        'imgs': [image.img.url for image in place.img.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+                    'lat': place.lat,
+                    'lng': place.lng,
         }
 
     }
