e291ff04234c446a3ec263b2a90735ac310961e0
Rinat <swatlprus@gmail.com>
2023-05-09 14:31:24 +0500

    Change name function

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import get_id_place
+from .views import get_desciptions_place
 
 app_name = 'places'
 urlpatterns = [
-    path('<int:place_id>/', get_id_place, name='place_details')
+    path('<int:place_id>/', get_desciptions_place, name='place_details')
 ]
